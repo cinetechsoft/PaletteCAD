0437ac6ac42cb1e59e4a313c6584bb3b957c7da4
Abhishek Tiwari <abhishektiwari9805@gmail.com>
2023-07-19 20:41:57 +0530

    Sidebar style fix

--- src/components/common/Layout/AppNavbar/index.tsx
+++ src/components/common/Layout/AppNavbar/index.tsx
@@ -1,14 +1,14 @@
-import { Navbar, NavLink, ChevronIcon } from '@mantine/core';
+import { Navbar, NavLink, ChevronIcon, Box } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { IconHome2 } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
 import routes, { RouteItem } from '../../../../routes';
 interface AppNavbarProps { opened: boolean; }
-const NavListItem = ({ route }: { route: RouteItem }) => {
-    return (<Link to={route.path} key={route.name} style={{
+const NavListItem = ({ route, opened }: { opened: boolean; route: RouteItem }) => {
+    return opened ? (<Link to={route.path} key={route.name} style={{
         all: `unset`
     }}> {route?.subRoutes ? route?.subRoutes.map(subRoute => <NavLink label={route.name} key={route.name} icon={<route.icon />} ><NavListItem route={subRoute} key={subRoute.name} /></NavLink>) : <NavLink label={route.name} key={route.name} icon={<route.icon />} />}
-    </Link>)
+    </Link>) : <Box m="8px 12px" display={'flex'}><route.icon style={{ minWidth: 24 }} /></Box>
 }
 function AppNavbar({ opened }: AppNavbarProps) {
     const [isExpanded, { open, close }] = useDisclosure(false)
@@ -16,7 +16,7 @@ function AppNavbar({ opened }: AppNavbarProps) {
         <Navbar p={opened ? "md" : "xs"} hiddenBreakpoint="sm" hidden={!opened} width={isExpanded ? { sm: 200, lg: 300 } : { sm: 50, lg: 60 }} style={{ transition: `all 0.5s` }}>
             <NavLink label={!opened ? "Colapse" : ""} icon={<ChevronIcon transform={opened ? `rotate(90)` : `rotate(270)`} />} onClick={isExpanded ? close : open} />
             {
-                routes.filter(e => !e?.isHidden).map(route => <NavListItem route={route} />)
+                routes.filter(e => !e?.isHidden).map(route => <NavListItem route={route} opened={isExpanded} />)
             }
 
         </Navbar>
